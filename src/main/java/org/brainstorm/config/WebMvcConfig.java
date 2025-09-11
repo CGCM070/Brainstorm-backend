93713b47241eb4f45aaa441285260ecfd27b89fc
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2025-09-11 22:36:02 +0200

    Simple security method

--- src/main/java/org/brainstorm/config/WebMvcConfig.java
+++ src/main/java/org/brainstorm/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package org.brainstorm.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig  implements WebMvcConfigurer {
 
-    @Autowired
-    private SecurityInterceptor securityInterceptor;
+    private final SecurityInterceptor securityInterceptor;
+
+    public WebMvcConfig(SecurityInterceptor securityInterceptor) {
+        this.securityInterceptor = securityInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
